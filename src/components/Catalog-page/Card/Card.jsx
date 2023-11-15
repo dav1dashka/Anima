5f7b67ddf9640b1b5acbc8b26aeaddf627a3550f
dav1dashka <dujadavyd17@gmail.com>
2023-11-16 00:16:22 +0200

    add: add files to public folder

--- src/components/Catalog-page/Card/Card.jsx
+++ src/components/Catalog-page/Card/Card.jsx
@@ -10,32 +10,32 @@ const Card = (props) => {
     return (
         <>
             <div className={cardClassName}>
-            <Fade
-                                    triggerOnce={true}
-                                    style={{ 'position': 'relative', 'zIndex': '1' }}
-                                >
-                <Link to={`/product/${id}`}>
-                    <div className="card__contain">
-                        <div className={discount ? 'card__image discount' : 'card__image'}>
-                            <img src={images[0]} alt="" />
-                            <img src={images[1]} alt="" />
-                        </div>
-                        <div className="card__cov">
-                            <div className="card__cover">
-                                <span className="card__label">{label}</span>
-                                <h3 className="card__title">{title}</h3>
-                                <div className="card__price">
-                                    <span className="card__cur-price">{curPrice} $</span>
-                                    <span className="card__prev-price">{prevPrice == null ? prevPrice : prevPrice + ' $'}</span>
+                <Fade
+                    triggerOnce={true}
+                    style={{ 'position': 'relative', 'zIndex': '1' }}
+                >
+                    <Link to={`/product/${id}`}>
+                        <div className="card__contain">
+                            <div className={discount ? 'card__image discount' : 'card__image'}>
+                                <img src={images[0]} alt="" />
+                                <img src={images[1]} alt="" />
+                            </div>
+                            <div className="card__cov">
+                                <div className="card__cover">
+                                    <span className="card__label">{label}</span>
+                                    <h3 className="card__title">{title}</h3>
+                                    <div className="card__price">
+                                        <span className="card__cur-price">{curPrice} $</span>
+                                        <span className="card__prev-price">{prevPrice == null ? prevPrice : prevPrice + ' $'}</span>
+                                    </div>
+                                </div>
+                                <div className="card__button">
+                                    <button type='button'>Детальніше    </button>
                                 </div>
                             </div>
-                            <div className="card__button">
-                                <button type='button'>Детальніше    </button>
-                            </div>
                         </div>
-                    </div>
-                </Link>
-            </Fade>
+                    </Link>
+                </Fade>
 
             </div>
         </>

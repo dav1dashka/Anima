1768c08cf0c6b469cfeb47fa381d8feae5c8be04
dav1dashka <dujadavyd17@gmail.com>
2023-11-15 23:17:19 +0200

    fixed path

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
 
-import { createBrowserRouter, RouterProvider, } from "react-router-dom";
+import { HashRouter as createBrowserRouter, RouterProvider } from "react-router-dom";
 
 
 import HomePage from '../Home-page/Home-page'
